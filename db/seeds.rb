5e0f7f6250f2f131bcfe57967a3663157956acbc
uchidataku <bbb.rhcp99@gmail.com>
2022-11-03 10:00:16 +0900

    feat: seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-account = Account.find_or_initialize_by(
+admin_account = Account.find_or_initialize_by(
   username: 'admin',
   is_system_admin: true
 )
-if account.new_record?
-  account.update!(password: 'password', password_confirmation: 'password')
+if admin_account.new_record?
+  admin_account.update!(password: 'password', password_confirmation: 'password')
 end
 
 accounts = []
@@ -16,16 +16,61 @@ accounts = []
 end
 Account.import accounts
 
+## 漫画
 ranking = Ranking.find_or_create_by!(
   title: '好きな漫画',
   genre: Ranking::Genre::ENTERTAINMENT,
-  creator: account
+  creator: admin_account
 )
 
 titles = %w[
 OnePiece 呪術廻戦 進撃の巨人 スラムダンク ハンターハンター
 キングダム ドラゴンボール チェンソーマン ナルト ヒカルの碁
 テニスの王子様 アイシールド21 SPYxFAMILY あひるの空 金色のガッシュ
+]
+
+likes = []
+titles.each do |title|
+  item = Item.find_or_create_by!(name: title, ranking: ranking)
+  rand(1..100).times.each do |n|
+    likes << Like.new(item: item, account: Account.find_by(username: "testuser_#{n}"))
+  end
+end
+Like.import likes
+
+## 映画
+ranking = Ranking.find_or_create_by!(
+  title: 'おすすめ洋画',
+  genre: Ranking::Genre::ENTERTAINMENT,
+  creator: admin_account
+)
+
+titles = %w[
+BackToTheFuture アルマゲドン グレイテストショーマン E.T. ショーシャンクの空に
+インセプション マトリックス ロードオブザリング バイオハザード マンマミーア
+ジュマンジ ジュラシックパーク ターミネーター インディージョーンズ ベンジャミンバトン
+]
+
+likes = []
+titles.each do |title|
+  item = Item.find_or_create_by!(name: title, ranking: ranking)
+  rand(1..100).times.each do |n|
+    likes << Like.new(item: item, account: Account.find_by(username: "testuser_#{n}"))
+  end
+end
+Like.import likes
+
+# プログラミング言語
+ranking = Ranking.find_or_create_by!(
+  title: '好きなプログラミング言語',
+  genre: Ranking::Genre::IT,
+  creator: admin_account
+)
+
+titles = %w[
+Ruby Rust Go Java C C# C++
+Perl PHP Python Swift Kotlin
+JavaScript TypeScript Dart Nim
 ]
 
 likes = []
