2be349b9d856af7e8d763aae6009f15f8e8f4ac3
uchidataku <bbb.rchp99@gmail.com>
2022-12-12 22:14:51 +0900

    feat: ヘルスチェック用エンドポイント追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/health', to: proc {
+    [200, {}, ['']]
+  }
+
   post :sign_in, to: 'auth#sign_in'
   post :sign_up, to: 'auth#sign_up'
   get :current_user, to: 'auth#current_user'
