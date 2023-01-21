aa25b1b4e25bc69e03712c39dfaf262096c0e406
uchidataku <bbb.rchp99@gmail.com>
2023-01-21 23:06:56 +0900

    feat: CIのdeployを一旦コメント

--- spec/requests/genre_categories_controller_spec.rb
+++ spec/requests/genre_categories_controller_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-# RSpec.describe GenreCategoriesController, type: :request do
-#   describe 'GET /genre_categories' do
-#     subject(:request) { get genre_categories_path }
-#
-#     it 'Ranking一覧を取得できる' do
-#       request
-#       expect(response).to have_http_status(:ok)
-#       parsed_body = JSON.parse(response.body)
-#       expect(parsed_body['genre_categories'].count).to eq 18
-#     end
-#   end
-# end
+RSpec.describe GenreCategoriesController, type: :request do
+  describe 'GET /genre_categories' do
+    subject(:request) { get genre_categories_path }
+
+    it 'Ranking一覧を取得できる' do
+      request
+      expect(response).to have_http_status(:ok)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['genre_categories'].count).to eq 18
+    end
+  end
+end
